79d83b73a6dd6475c053c8afb36e1adf13b5c90b
Callum McIntyre <callum@pointer.gg>
2022-07-18 21:02:06 +0100

    Account fetch: add input validation + error handling (#41)
    * Add input validation on account address
    
    * Also disable fetch account buttons on validation error/disconnected wallet
    
    * Handle RPC errors when fetching account data

--- client/src/components/Panels/Side/Right/Test/FetchableAccount.tsx
+++ client/src/components/Panels/Side/Right/Test/FetchableAccount.tsx
@@ -1,7 +1,7 @@
 import { BN, Idl } from "@project-serum/anchor";
 import { useConnection } from "@solana/wallet-adapter-react";
 import { PublicKey } from "@solana/web3.js";
-import { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import styled, { css } from "styled-components";
 import { PgAccount } from "../../../../../utils/pg/account";
 import Button from "../../../../Button";
@@ -32,7 +32,9 @@ const FetchableAccountInside = ({ accountName, idl }: FetchableAccountInsideProp
   const { currentWallet } = useCurrentWallet();
 
   const [enteredAddress, setEnteredAddress] = useState("");
+  const [enteredAddressError, setEnteredAddressError] = useState(false);
   const [fetchedData, setFetchedData] = useState<any>();
+  const [fetchError, setFetchError] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     // The default BN.toJSON is a hex string, but we want a readable string
@@ -46,35 +48,59 @@ const FetchableAccountInside = ({ accountName, idl }: FetchableAccountInsideProp
     return () => { BN.prototype.toJSON = oldBNPrototypeToJSON }
   }, [])
 
+  const handleError = (err: any) => {
+    if (err instanceof Error && err.message.startsWith("Account does not exist")) {
+      setFetchError(err.message);
+    } else {
+      console.error(err);
+      setFetchError("Unknown error fetching account data");
+    }
+  }
+
+  const handleFetched = (data: any) => {
+    setFetchedData(data);
+    setFetchError(undefined);
+  }
+
   const fetchAll = async () => {
     if (!currentWallet) return;
-    setFetchedData(null);
-    const accountData = await PgAccount.fetchAll(accountName, idl, conn, currentWallet);
-    console.log({ accountData });
-    setFetchedData(accountData);
+    try {
+      const accountData = await PgAccount.fetchAll(accountName, idl, conn, currentWallet);
+      handleFetched(accountData);
+    } catch (err: any) {
+      handleError(err);
+    }
   }
 
   const fetchEntered = async () => {
     if (!currentWallet) return;
-    setFetchedData(null);
-    const accountData = await PgAccount.fetchOne(accountName, new PublicKey(enteredAddress), idl, conn, currentWallet);
-    console.log({ accountData: accountData.data.toString() });
-    setFetchedData(accountData);
+    try {
+      const accountData = await PgAccount.fetchOne(accountName, new PublicKey(enteredAddress), idl, conn, currentWallet);
+      handleFetched(accountData);
+    } catch (err: any) {
+      handleError(err);
+    }
   }
 
-  return (
-    <>
-      <InputWrapper>
-        <InputLabel label="address" type="publicKey" />
-        <Input type="text" value={enteredAddress} onChange={e => setEnteredAddress(e.target.value)} {...defaultInputProps} />
-      </InputWrapper>
+  const enteredAddressChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const address = e.target.value;
+    if (address) {
+      try {
+        new PublicKey(address);
+        setEnteredAddressError(false);
+      } catch {
+        setEnteredAddressError(true);
+      }
+    }
 
-      <ButtonsWrapper>
-        <Button onClick={fetchEntered} disabled={!enteredAddress} kind="outline" fullWidth={false} size="small">Fetch Entered</Button>
-        <Button onClick={fetchAll} kind="outline" fullWidth={false} size="small">Fetch All</Button>
-      </ButtonsWrapper>
+    setEnteredAddress(address);
+  }
 
-      {fetchedData && (
+  const renderResult = () => {
+    if (fetchError) {
+      return <ErrorWrapper>{fetchError}</ErrorWrapper>;
+    } else if (fetchedData) {
+      return (
         <ResultWrapper>
           <Foldable ClickEl={<span>Result</span>} open>
             <Result>
@@ -82,7 +108,47 @@ const FetchableAccountInside = ({ accountName, idl }: FetchableAccountInsideProp
             </Result>
           </Foldable>
         </ResultWrapper>
-      )}
+      );
+    } else {
+      return null;
+    }
+  }
+
+  return (
+    <>
+      <InputWrapper>
+        <InputLabel label="address" type="publicKey" />
+        <Input
+          type="text"
+          className={enteredAddressError ? 'error' : ''}
+          value={enteredAddress}
+          onChange={enteredAddressChanged}
+          {...defaultInputProps}
+        />
+      </InputWrapper>
+
+      <ButtonsWrapper>
+        <Button
+          onClick={fetchEntered}
+          disabled={!enteredAddress || enteredAddressError || !currentWallet}
+          kind="outline"
+          fullWidth={false}
+          size="small"
+        >
+          Fetch Entered
+        </Button>
+        <Button
+          onClick={fetchAll}
+          disabled={!currentWallet}
+          kind="outline"
+          fullWidth={false}
+          size="small"
+        >
+          Fetch All
+        </Button>
+      </ButtonsWrapper>
+
+      {renderResult()}
     </>
   )
 }
@@ -119,6 +185,14 @@ const ButtonsWrapper = styled.div`
 
 const ResultWrapper = styled.div`
     margin: 0.5rem 0;
+`;
+
+const ErrorWrapper = styled.div`
+  ${({ theme }) => css`
+    margin: 0.5rem 0;
+    color: ${theme.colors.state.error.color};
+    user-select: text;
+  `}
 `;
 
 const Result = styled.pre`
