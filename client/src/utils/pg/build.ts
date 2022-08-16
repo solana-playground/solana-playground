0b9fb516e54e0d6c982c4a38dc14b871f9c652fc
Callum McIntyre <callum@pointer.gg>
2022-08-16 15:40:25 +0100

    Update build to seahorse compile python files before calling build API

--- client/src/utils/pg/build.ts
+++ client/src/utils/pg/build.ts
@@ -3,6 +3,7 @@ import { Idl } from "@project-serum/anchor";
 import { SERVER_URL } from "../../constants";
 import { PgCommon } from "./common";
 import { PgProgramInfo } from "./program-info";
+import { PgPkg, PkgName } from "./terminal";
 
 interface BuildResp {
   stderr: string;
@@ -13,8 +14,41 @@ interface BuildResp {
 export type Files = string[][];
 
 export class PgBuild {
+  static getProgramName(fileName: string, expectedSuffix: string) {
+    const pathParts = fileName.split("/");
+    const lastPart = pathParts[pathParts.length - 1];
+    return lastPart.replace(expectedSuffix, "");
+  }
+
   static async build(files: Files) {
     const programInfo = PgProgramInfo.getProgramInfo();
+
+    const pythonFiles = files.filter(([fileName]) =>
+      fileName.toLowerCase().endsWith(".py")
+    );
+
+    let rustFiles: Files;
+    if (pythonFiles.length > 0) {
+      const seahorsePkg = await PgPkg.loadPkg(PkgName.SEAHORSE_COMPILE);
+      const compileFn = seahorsePkg.compileSeahorse;
+      if (!compileFn) {
+        throw new Error("No compile function found in seahorse package");
+      }
+
+      rustFiles = files.map((file) => {
+        const [fileName, contents] = file;
+        const newFileName = fileName.replace(".py", ".rs");
+        const programName = this.getProgramName(fileName, ".py");
+        const newContents = compileFn(contents, programName);
+        if (newContents.length === 0) {
+          throw new Error("Seahorse compile failed");
+        }
+
+        return [newFileName, newContents];
+      });
+    } else {
+      rustFiles = files;
+    }
 
     const resp = await fetch(`${SERVER_URL}/build`, {
       method: "POST",
@@ -22,7 +56,7 @@ export class PgBuild {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        files,
+        files: rustFiles,
         uuid: programInfo.uuid,
       }),
     });
