d50187387a28d9ecf6b24683134f1ea6db37e8d9
acheron <acheroncrypto@gmail.com>
2022-09-04 05:20:23 +0200

    Add error state for rename workspace

--- client/src/components/Panels/Side/Right/Explorer/Modals/RenameWorkspace.tsx
+++ client/src/components/Panels/Side/Right/Explorer/Modals/RenameWorkspace.tsx
@@ -1,11 +1,12 @@
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useAtom } from "jotai";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import ModalInside from "../../../../../Modal/ModalInside";
 import useModal from "../../../../../Modal/useModal";
 import Input, { defaultInputProps } from "../../../../../Input";
 import { explorerAtom } from "../../../../../../state";
+import { ClassName } from "../../../../../../constants";
 
 export const RenameWorkspace = () => {
   const [explorer] = useAtom(explorerAtom);
@@ -14,41 +15,54 @@ export const RenameWorkspace = () => {
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  // Focus input on mount
+  // Focus and select input on mount
   useEffect(() => {
     inputRef.current?.select();
     inputRef.current?.focus();
   }, []);
 
   const workspaceName = explorer?.currentWorkspaceName ?? "";
+
   // Handle user input
   const [newName, setNewName] = useState(workspaceName);
+  const [error, setError] = useState("");
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setNewName(e.target.value);
+    setError("");
   };
 
+  const disableCond = !newName || !!error || !explorer;
+
   const renameWorkspace = async () => {
-    if (!newName || !explorer) return;
+    if (disableCond) return;
 
     try {
       await explorer.renameWorkspace(newName);
       close();
     } catch (e: any) {
-      console.log(e.message);
+      setError(e.message);
     }
   };
 
   return (
     <ModalInside
-      buttonProps={{ name: "Rename", onSubmit: renameWorkspace }}
+      buttonProps={{
+        name: "Rename",
+        onSubmit: renameWorkspace,
+        disabled: disableCond,
+        size: "small",
+      }}
       title={`Rename workspace '${workspaceName}'`}
       closeOnSubmit={false}
     >
       <Content>
+        {error && <ErrorText>{error}</ErrorText>}
         <Input
           ref={inputRef}
           onChange={handleChange}
           value={newName}
+          className={error ? ClassName.ERROR : ""}
           {...defaultInputProps}
         />
       </Content>
@@ -61,10 +75,21 @@ const Content = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 1.5rem 0;
+  margin: 1rem 0 1.5rem 0;
 
   & > input {
     font-size: ${({ theme }) => theme.font?.size.medium};
     padding: 0.375rem 0.5rem;
   }
+`;
+
+const ErrorText = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.state.error.color};
+    font-size: ${theme.font?.size.small};
+    margin-bottom: 0.5rem;
+    display: flex;
+    justify-content: flex-start;
+    width: 100%;
+  `}
 `;
