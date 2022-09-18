9fea0cb3c29144d9dbca85d6732797d114508ed4
acheron <acheroncrypto@gmail.com>
2022-09-18 10:40:30 +0200

    Fix webpack build warnings

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,7 +1,9 @@
+const webpack = require("webpack");
+
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      const wasmExtensionRegExp = /\.wasm$/;
+      // Resolve .wasm
       webpackConfig.resolve.extensions.push(".wasm");
       webpackConfig.experiments = {
         asyncWebAssembly: true,
@@ -9,10 +11,29 @@ module.exports = {
       webpackConfig.module.rules.forEach((rule) => {
         (rule.oneOf || []).forEach((oneOf) => {
           if (oneOf.type === "asset/resource") {
-            oneOf.exclude.push(wasmExtensionRegExp);
+            oneOf.exclude.push(/\.wasm$/);
           }
         });
       });
+
+      // Resolve node polyfills
+      webpackConfig.resolve.fallback = {
+        // Fix `Module not found: Error: Can't resolve 'perf_hooks'` from typescript
+        perf_hooks: false,
+      };
+
+      // Plugins
+      webpackConfig.plugins.push(
+        // Ignore `Critical dependency: the request of a dependency is an expression`
+        // from typescript
+        new webpack.ContextReplacementPlugin(/^\.$/, (context) => {
+          if (/\/node_modules\/typescript\/lib/.test(context.context)) {
+            for (const d of context.dependencies) {
+              if (d.critical) d.critical = false;
+            }
+          }
+        })
+      );
 
       return webpackConfig;
     },
