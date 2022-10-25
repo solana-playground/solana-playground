186ec6c8dcc3fe10ffc8d9018b8b1443e5a1fbfc
Ajay Vasisht <43521356+avasisht23@users.noreply.github.com>
2022-10-24 21:50:14 -0400

    Update connection.ts

--- client/src/constants/connection.ts
+++ client/src/constants/connection.ts
@@ -4,23 +4,23 @@ export enum NetworkName {
   LOCALHOST = "localhost",
   DEVNET = "devnet",
   DEVNET_GENESYSGO = "devnet-genesysgo",
+  DEVNET_ALCHEMY = "devnet-alchemy",
   TESTNET = "testnet",
   MAINNET_BETA = "mainnet-beta",
   MAINNET_BETA_GENESYSGO = "mainnet-beta-genesysgo",
   MAINNET_BETA_SERUM = "mainnet-beta-serum",
   CUSTOM = "custom",
-  DEVNET_ALCHEMY = "devnet-alchemy",
 }
 
 export enum Endpoint {
   LOCALHOST = "http://localhost:8899",
   DEVNET = "https://api.devnet.solana.com",
   DEVNET_GENESYSGO = "https://devnet.genesysgo.net/",
+  DEVNET_ALCHEMY = "https://solana-devnet.g.alchemy.com/v2/demo",
   TESTNET = "https://api.testnet.solana.com",
   MAINNET_BETA = "https://api.mainnet-beta.solana.com",
   MAINNET_BETA_GENESYSGO = "https://ssc-dao.genesysgo.net",
   MAINNET_BETA_SERUM = "https://solana-api.projectserum.com",
-  DEVNET_ALCHEMY = "https://solana-devnet.g.alchemy.com/v2/demo",
 }
 
 interface Network {
@@ -42,6 +42,10 @@ export const NETWORKS: Network[] = [
     endpoint: Endpoint.DEVNET_GENESYSGO,
   },
   {
+    name: NetworkName.DEVNET_ALCHEMY,
+    endpoint: Endpoint.DEVNET_ALCHEMY,
+  },
+  {
     name: NetworkName.TESTNET,
     endpoint: Endpoint.TESTNET,
   },
@@ -56,10 +60,6 @@ export const NETWORKS: Network[] = [
   {
     name: NetworkName.MAINNET_BETA_SERUM,
     endpoint: Endpoint.MAINNET_BETA_SERUM,
-  },
-  {
-    name: NetworkName.DEVNET_ALCHEMY,
-    endpoint: Endpoint.DEVNET_ALCHEMY,
   },
 ];
 
