3ea63a8b1723edddeab3639c066246cbe77240b4
C <caaarloos300@gmail.com>
2022-10-28 12:28:22 +0900

    Adding information of the Counter tutorial

--- client/src/tutorials/Template_counter/files/lib.rs
+++ client/src/tutorials/Template_counter/files/lib.rs
@@ -1,20 +1,19 @@
+
 use anchor_lang::prelude::*;
 
 declare_id!("9MGFR88uofDVcjHx3hoy2jmRtcnfMdQUbYRHCFFJFuXC");
 
 #[program]
-
+// Smart contract functions
 pub mod counter {
     use super::*;
 
     pub fn create_counter(ctx: Context<CreateCounter>) -> Result<()> {
-
         msg!("Creating a Counter!!");
 
-        let counter = &mut ctx.accounts.counter;
-        counter.authority = ctx.accounts.authority.key();
-        counter.count = 0;
+         // The creation of the counter must be here
 
+        
         msg!("Current count is {}", counter.count);
         msg!("The Admin PubKey is: {} ", counter.authority);
 
@@ -22,14 +21,12 @@ pub mod counter {
     }
 
     pub fn update_counter(ctx: Context<UpdateCounter>) -> Result<()> {
-
         msg!("Adding 1 to the counter!!");
 
-        let counter = &mut ctx.accounts.counter;
-        counter.count += 1 ;
+        // Updating the counter must be here 
 
         msg!("Current count is {}", counter.count);
-        msg!("{} remaining to reach 1000 ", 1000-counter.count);
+        msg!("{} remaining to reach 1000 ", 1000 - counter.count);
 
         Ok(())
     }
@@ -37,7 +34,6 @@ pub mod counter {
 }
 
 // Data validators
-
 #[derive(Accounts)]
 pub struct CreateCounter<'info> {
     #[account(mut)]
@@ -60,3 +56,66 @@ pub struct Counter {
     authority: Pubkey,
     count: u64,
 }
+
+// use anchor_lang::prelude::*;
+
+// declare_id!("9MGFR88uofDVcjHx3hoy2jmRtcnfMdQUbYRHCFFJFuXC");
+
+// #[program]
+
+// pub mod counter {
+//     use super::*;
+
+//     pub fn create_counter(ctx: Context<CreateCounter>) -> Result<()> {
+
+//         msg!("Creating a Counter!!");
+
+//         let counter = &mut ctx.accounts.counter;
+//         counter.authority = ctx.accounts.authority.key();
+//         counter.count = 0;
+
+//         msg!("Current count is {}", counter.count);
+//         msg!("The Admin PubKey is: {} ", counter.authority);
+
+//         Ok(())
+//     }
+
+//     pub fn update_counter(ctx: Context<UpdateCounter>) -> Result<()> {
+
+//         msg!("Adding 1 to the counter!!");
+
+//         let counter = &mut ctx.accounts.counter;
+//         counter.count += 1 ;
+
+//         msg!("Current count is {}", counter.count);
+//         msg!("{} remaining to reach 1000 ", 1000-counter.count);
+
+//         Ok(())
+//     }
+
+// }
+
+// // Data validators
+
+// #[derive(Accounts)]
+// pub struct CreateCounter<'info> {
+//     #[account(mut)]
+//     authority: Signer<'info>,
+//     #[account(init, seeds=[authority.key().as_ref()], bump, payer=authority, space=100)]
+//     counter: Account<'info, Counter>,
+//     system_program: Program<'info, System>,
+// }
+
+// #[derive(Accounts)]
+// pub struct UpdateCounter<'info> {
+//     authority: Signer<'info>,
+//     #[account(mut, has_one = authority)]
+//     counter: Account<'info, Counter>,
+// }
+
+// // Data structures
+// #[account]
+// pub struct Counter {
+//     authority: Pubkey,
+//     count: u64,
+// }
