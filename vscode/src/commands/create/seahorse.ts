af8bd48c258467a6d3a3fceb5ee9b3f28d90f7ce
Callum McIntyre <mcintyre1994@gmail.com>
2022-11-29 17:11:47 +0000

    vscode: fix seahorse create (#89)
    simplify to write seahorse files directly instead of calling processCreate again

--- vscode/src/commands/create/seahorse.ts
+++ vscode/src/commands/create/seahorse.ts
@@ -1,48 +1,46 @@
 import * as path from "path";
 
 import { PATHS } from "../../constants";
+import { Files, PgFs } from "../../utils";
 import { processCreateAnchor } from "./anchor";
-import { processCreate } from "./common";
 
 export const processCreateSeahorse = async () => {
   // Create Anchor
-  const { name } = await processCreateAnchor();
+  const { name, projectUri } = await processCreateAnchor();
 
-  // Create Seahorse
-  await processCreate({
-    name,
-    files: [
-      // Program <name>.py
-      [
-        path.join(PATHS.DIRS.PROGRAMS_PY, `${name}.py`),
-        `# ${name}
+  const seahorseFiles: Files = [
+    // Program <name>.py
+    [
+      path.join(PATHS.DIRS.PROGRAMS_PY, `${name}.py`),
+      `# ${name}
 # Built with Seahorse v0.2.3
 
 from seahorse.prelude import *
 
 declare_id('Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS')
 `,
-      ],
-
-      // __init__.py
-      [
-        path.join(
-          PATHS.DIRS.PROGRAMS_PY,
-          PATHS.DIRS.SEAHORSE,
-          PATHS.FILES.INIT_PY
-        ),
-        "",
-      ],
-
-      // prelude.py
-      [
-        path.join(
-          PATHS.DIRS.PROGRAMS_PY,
-          PATHS.DIRS.SEAHORSE,
-          PATHS.FILES.PRELUDE_PY
-        ),
-        require("./seahorse-prelude.py"),
-      ],
     ],
-  });
+
+    // __init__.py
+    [
+      path.join(
+        PATHS.DIRS.PROGRAMS_PY,
+        PATHS.DIRS.SEAHORSE,
+        PATHS.FILES.INIT_PY
+      ),
+      "",
+    ],
+
+    // prelude.py
+    [
+      path.join(
+        PATHS.DIRS.PROGRAMS_PY,
+        PATHS.DIRS.SEAHORSE,
+        PATHS.FILES.PRELUDE_PY
+      ),
+      require("./seahorse-prelude.py"),
+    ],
+  ];
+
+  await PgFs.writeFiles(seahorseFiles, projectUri);
 };
