ff3db7a6a1cc0aead5b462285dce5064d112de56
acheron <acheroncrypto@gmail.com>
2022-12-04 10:24:32 +0100

    Hide template tutorial

--- client/src/tutorials/tutorials.ts
+++ client/src/tutorials/tutorials.ts
@@ -8,21 +8,6 @@ const getTutorialImgSrc = (src: string) => "/tutorials/" + src;
 
 export const TUTORIALS: TutorialData[] = [
   {
-    name: "Template Tutorial",
-    description: "Simple template tutorial.",
-    imageSrc: getTutorialImgSrc("template/thumbnail.png"),
-    authors: [
-      {
-        name: "acheron",
-        link: "https://twitter.com/acheroncrypto",
-      },
-    ],
-    level: TutorialLevel.BEGINNER,
-    categories: [TutorialCategory.OTHER],
-    elementImport: () => import("./Template"),
-  },
-
-  {
     name: "Hello Solana",
     description: "Hello world program with Native Solana/Rust.",
     imageSrc: getTutorialImgSrc("hello-solana/thumbnail.jpg"),
