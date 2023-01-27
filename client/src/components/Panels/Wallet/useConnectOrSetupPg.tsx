1ce27f8bc2a64c45402bb8f5ad9286e748396932
acheron <acheroncrypto@gmail.com>
2023-01-27 21:29:18 +0100

    Fix terminal getting stuck at disabled state after cancelling the wallet setup

--- client/src/components/Panels/Wallet/useConnectOrSetupPg.tsx
+++ client/src/components/Panels/Wallet/useConnectOrSetupPg.tsx
@@ -2,33 +2,30 @@ import { useCallback } from "react";
 import { useAtom } from "jotai";
 
 import Setup from "./Setup";
-import { modalAtom, pgWalletAtom, refreshPgWalletAtom } from "../../../state";
-import { PgTerminal, PgWallet } from "../../../utils/pg";
+import { pgWalletAtom, refreshPgWalletAtom } from "../../../state";
+import { PgModal, PgTerminal, PgWallet } from "../../../utils/pg";
 
 export const useConnectOrSetupPg = () => {
   const [pgWallet] = useAtom(pgWalletAtom);
   const [pgWalletChanged, refresh] = useAtom(refreshPgWalletAtom);
-  const [, setModal] = useAtom(modalAtom);
 
   // Pg wallet should always be connected except first time ever
   const handleConnectPg = useCallback(() => {
-    const setupCompleted = PgWallet.getLs()?.setupCompleted;
-    if (!setupCompleted) setModal(<Setup onSubmit={handleConnectPg} />);
-    else {
-      pgWallet.connected = !pgWallet.connected;
-      PgWallet.update({ connected: pgWallet.connected });
-      refresh();
-      PgTerminal.disable();
-      PgTerminal.log(
-        pgWallet.connected
-          ? PgTerminal.success("Connected.")
-          : PgTerminal.bold("Disconnected.")
-      );
-
-      setTimeout(() => {
-        PgTerminal.enable();
-      }, 50);
-    }
+    PgTerminal.runCmd(async () => {
+      const setupCompleted = PgWallet.getLs()?.setupCompleted;
+      if (!setupCompleted) {
+        await PgModal.set(() => <Setup onSubmit={handleConnectPg} />);
+      } else {
+        pgWallet.connected = !pgWallet.connected;
+        PgWallet.update({ connected: pgWallet.connected });
+        refresh();
+        PgTerminal.log(
+          pgWallet.connected
+            ? PgTerminal.success("Connected.")
+            : PgTerminal.bold("Disconnected.")
+        );
+      }
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pgWalletChanged]);
 
