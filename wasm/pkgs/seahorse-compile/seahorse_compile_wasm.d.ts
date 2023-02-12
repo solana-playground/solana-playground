27c6d4f103cf707f65e86ba7e957416c03bf87d3
acheron <acheroncrypto@gmail.com>
2023-02-12 18:51:17 +0100

    Update Seahorse to 0.2.7

--- wasm/pkgs/seahorse-compile/seahorse_compile_wasm.d.ts
+++ wasm/pkgs/seahorse-compile/seahorse_compile_wasm.d.ts
@@ -6,3 +6,73 @@
 * @returns {any[]}
 */
 export function compileSeahorse(python_source: string, program_name: string): any[];
+/**
+* Handler for `console.log` invocations.
+*
+* If a test is currently running it takes the `args` array and stringifies
+* it and appends it to the current output of the test. Otherwise it passes
+* the arguments to the original `console.log` function, psased as
+* `original`.
+* @param {Array<any>} args
+*/
+export function __wbgtest_console_log(args: Array<any>): void;
+/**
+* Handler for `console.debug` invocations. See above.
+* @param {Array<any>} args
+*/
+export function __wbgtest_console_debug(args: Array<any>): void;
+/**
+* Handler for `console.info` invocations. See above.
+* @param {Array<any>} args
+*/
+export function __wbgtest_console_info(args: Array<any>): void;
+/**
+* Handler for `console.warn` invocations. See above.
+* @param {Array<any>} args
+*/
+export function __wbgtest_console_warn(args: Array<any>): void;
+/**
+* Handler for `console.error` invocations. See above.
+* @param {Array<any>} args
+*/
+export function __wbgtest_console_error(args: Array<any>): void;
+/**
+* Runtime test harness support instantiated in JS.
+*
+* The node.js entry script instantiates a `Context` here which is used to
+* drive test execution.
+*/
+export class WasmBindgenTestContext {
+  free(): void;
+/**
+* Creates a new context ready to run tests.
+*
+* A `Context` is the main structure through which test execution is
+* coordinated, and this will collect output and results for all executed
+* tests.
+*/
+  constructor();
+/**
+* Inform this context about runtime arguments passed to the test
+* harness.
+*
+* Eventually this will be used to support flags, but for now it's just
+* used to support test filters.
+* @param {any[]} args
+*/
+  args(args: any[]): void;
+/**
+* Executes a list of tests, returning a promise representing their
+* eventual completion.
+*
+* This is the main entry point for executing tests. All the tests passed
+* in are the JS `Function` object that was plucked off the
+* `WebAssembly.Instance` exports list.
+*
+* The promise returned resolves to either `true` if all tests passed or
+* `false` if at least one test failed.
+* @param {any[]} tests
+* @returns {Promise<any>}
+*/
+  run(tests: any[]): Promise<any>;
+}
