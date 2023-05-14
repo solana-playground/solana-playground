df551a5c29fb8c6e5de95902314a4c130ef18283
GitBolt <saabisakhtar@gmail.com>
2023-05-15 01:07:29 +0530

    format: code, update: tutorial

--- client/src/tutorials/ExpenseTracker/files/index.test.ts.raw
+++ client/src/tutorials/ExpenseTracker/files/index.test.ts.raw
@@ -2,7 +2,8 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Etracker } from "../target/types/etracker";
 
-describe("etracker", async() => {
+describe("Expense Tracker", async() => {
+
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
   const provider = anchor.AnchorProvider.local();
@@ -11,63 +12,62 @@ describe("etracker", async() => {
 
   const wallet = provider.wallet as anchor.Wallet;
 
-  let mname = "test";
-  let mamount = 100;
+  let merchant_name = "test";
+  let amount = 100;
+  let id = "1"
 
-  let mname2 = "test2";
-  let mamount2 = 200;
+  let merchant_name2 = "test 2";
+  let amount2 = 200;
 
-  let [expense_account,vPDA1] = await anchor.web3.PublicKey.findProgramAddress(
-    [Buffer.from("expense"), wallet.publicKey.toBuffer(), Buffer.from("1")],
+  let [expense_account] = await anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("expense"), wallet.publicKey.toBuffer(), Buffer.from(id)],
     program.programId
   );
 
   it("Initialize Expense", async () => {
-    // Add your test here.
 
-    const tx = await program.methods.initializeExpense(
-      "1",
-      mname,
-      new anchor.BN(mamount)
+    await program.methods.initializeExpense(
+      id,
+      merchant_name,
+      new anchor.BN(amount)
     ).accounts({
       expenseAccount: expense_account,
       authority: wallet.publicKey,
     })
     .rpc();
 
-    let dt = await program.account.expenseAccount.fetch(expense_account);
-    await console.log(dt)
+    let data = await program.account.expenseAccount.fetch(expense_account);
+    console.log(data)
 
   });
 
   it("Modify Expense", async () => {
     // Add your test here.
 
-    const tx = await program.methods.modifyExpense(
-      "1",
-      mname2,
-      new anchor.BN(mamount2)
+    await program.methods.modifyExpense(
+      id,
+      merchant_name2,
+      new anchor.BN(amount2)
     ).accounts({
       expenseAccount: expense_account,
       authority: wallet.publicKey,
     })
     .rpc();
 
-    let dt = await program.account.expenseAccount.fetch(expense_account);
-    await console.log(dt)
+    let data = await program.account.expenseAccount.fetch(expense_account);
+    console.log(data)
 
   });
 
   it("Delete Expense", async () => {
 
-    const tx = await program.methods.deleteExpense(
-      "1"
+    await program.methods.deleteExpense(
+      id
     ).accounts({
       expenseAccount: expense_account,
       authority: wallet.publicKey,
     })
     .rpc();
-
 
   });
 
--- client/src/tutorials/ExpenseTracker/files/lib.rs
+++ client/src/tutorials/ExpenseTracker/files/lib.rs
@@ -1,18 +1,19 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::{self,system_program, sysvar::rent::Rent,};
+use anchor_lang::solana_program::{self, system_program, sysvar::rent::Rent};
 
-// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 declare_id!("Dt1Cu5jz7UMzLFgXPNyYcP8jCcySYgLW5QXpubjKvvm3");
-
-
 
 #[program]
 pub mod etracker {
 
     use super::*;
 
-    pub fn initialize_expense(ctx: Context<InitializeExpense>,id : String,mname : String,amount : u64) -> Result<()> {
-        
+    pub fn initialize_expense(
+        ctx: Context<InitializeExpense>,
+        id: String,
+        merchant_name: String,
+        amount: u64,
+    ) -> Result<()> {
         let expense_account = &mut ctx.accounts.expense_account;
         let integer_id = id.parse::<u64>().unwrap();
         let string_id = integer_id.to_string();
@@ -20,119 +21,87 @@ pub mod etracker {
         require_eq!(string_id, id);
 
         expense_account.id = integer_id;
-        expense_account.mname = mname;
+        expense_account.merchant_name = merchant_name;
         expense_account.amount = amount;
         expense_account.owner = *ctx.accounts.authority.key;
-        expense_account.bump = *ctx.bumps.get("expense_account").unwrap();
 
         Ok(())
     }
 
-    
-    pub fn modify_expense(ctx: Context<ModifyExpense>,id : String,mname : String,amount : u64) -> Result<()> {
-        
+    pub fn modify_expense(
+        ctx: Context<ModifyExpense>,
+        id: String,
+        merchant_name: String,
+        amount: u64,
+    ) -> Result<()> {
         let expense_account = &mut ctx.accounts.expense_account;
-        expense_account.mname = mname;
+        expense_account.merchant_name = merchant_name;
         expense_account.amount = amount;
 
         Ok(())
     }
 
-    pub fn delete_expense(ctx: Context<DeleteExpense>,id : String) -> Result<()> {
-
+    pub fn delete_expense(ctx: Context<DeleteExpense>, id: String) -> Result<()> {
         Ok(())
     }
-
-
 }
 
 #[derive(Accounts)]
 #[instruction(id : String)]
 pub struct InitializeExpense<'info> {
-
-    #[account(
-        mut,
-    )]
+    #[account(mut)]
     pub authority: Signer<'info>,
-
 
     #[account(
         init,
         payer = authority,
-        space = 8 
-            + 8 // id
-            + 32 // owner
-            + (4 + 12) // merchant name
-            + 8 // amount
-            + 1, // bump
-        seeds = [b"expense".as_ref(),authority.key().as_ref(),id.as_ref()], 
+        space = 8 + 8 + 32+ (4 + 12)+ 8 + 1,
+        seeds = [b"expense", authority.key().as_ref(), id.as_ref()], 
         bump
     )]
     pub expense_account: Account<'info, ExpenseAccount>,
 
-    // Misc Accounts
-    #[account(address = system_program::ID)]
-    pub system_program: Program<'info,System>,
-    #[account(address = solana_program::sysvar::rent::ID)]
-    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 #[instruction(id : String)]
 pub struct ModifyExpense<'info> {
-
-    #[account(
-        mut,
-    )]
+    #[account(mut)]
     pub authority: Signer<'info>,
 
-
     #[account(
         mut,
-        seeds = [b"expense".as_ref(),authority.key().as_ref(),id.as_ref()], 
-        bump=expense_account.bump
+        seeds = [b"expense", authority.key().as_ref(), id.as_ref()], 
+        bump
     )]
     pub expense_account: Account<'info, ExpenseAccount>,
 
-    // Misc Accounts
-    #[account(address = system_program::ID)]
-    pub system_program: Program<'info,System>,
-    #[account(address = solana_program::sysvar::rent::ID)]
-    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 #[instruction(id : String)]
 pub struct DeleteExpense<'info> {
-
-    #[account(
-        mut,
-    )]
+    #[account(mut)]
     pub authority: Signer<'info>,
-
 
     #[account(
         mut,
         close = authority,
-        seeds = [b"expense".as_ref(),authority.key().as_ref(),id.as_ref()], 
-        bump=expense_account.bump
+        seeds = [b"expense", authority.key().as_ref(), id.as_ref()], 
+        bump
     )]
     pub expense_account: Account<'info, ExpenseAccount>,
 
-    // Misc Accounts
-    #[account(address = system_program::ID)]
-    pub system_program: Program<'info,System>,
-    #[account(address = solana_program::sysvar::rent::ID)]
-    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
 }
-
 
 #[account]
 #[derive(Default)]
 pub struct ExpenseAccount {
     pub id: u64,
     pub owner: Pubkey,
-    pub mname: String,
+    pub merchant_name: String,
     pub amount: u64,
-    pub bump: u8,
 }
