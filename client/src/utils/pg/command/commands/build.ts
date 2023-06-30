6e4b0622ab07376c72fbb691f47a706866c3fca3
acheron <acheroncrypto@gmail.com>
2023-06-30 10:34:00 +0200

    Move `PgExplorer.getBuildFiles` to inside `build` command

--- client/src/utils/pg/command/commands/build.ts
+++ client/src/utils/pg/command/commands/build.ts
@@ -1,9 +1,11 @@
+import { Keypair } from "@solana/web3.js";
+
 import { createCmd } from "../create-command";
 import { PgPackage } from "../package";
-import { PgTerminal } from "../../terminal";
-import { TupleFiles, PgExplorer } from "../../explorer";
+import { TupleFiles, PgExplorer, ExplorerFiles } from "../../explorer";
 import { PgProgramInfo } from "../../program-info";
 import { PgServer } from "../../server";
+import { PgTerminal } from "../../terminal";
 import { TerminalAction } from "../../../../state";
 
 export const build = createCmd({
@@ -33,7 +35,7 @@ export const build = createCmd({
  * @returns build output from stderr(not only errors)
  */
 const processBuild = async () => {
-  const buildFiles = PgExplorer.getBuildFiles();
+  const buildFiles = getBuildFiles();
   const pythonFiles = buildFiles.filter(([fileName]) =>
     fileName.toLowerCase().endsWith(".py")
   );
@@ -98,4 +100,131 @@ const buildPython = async (pythonFiles: TupleFiles) => {
   });
 
   return await buildRust(rustFiles);
+};
+
+/**
+ * Get the files necessary for the build process.
+ *
+ * @returns the necessary data for the build request
+ */
+const getBuildFiles = () => {
+  let programPkStr = PgProgramInfo.getPkStr();
+  if (!programPkStr) {
+    const kp = Keypair.generate();
+    PgProgramInfo.update({ kp });
+    programPkStr = kp.publicKey.toBase58();
+  }
+
+  const updateIdRust = (content: string) => {
+    let updated = false;
+
+    const rustDeclareIdRegex = new RegExp(
+      /^(([\w]+::)*)declare_id!\("(\w*)"\)/gm
+    );
+    const newContent = content.replace(rustDeclareIdRegex, (match) => {
+      const res = rustDeclareIdRegex.exec(match);
+      if (!res) return match;
+      updated = true;
+
+      // res[1] could be solana_program:: or undefined
+      return (res[1] ?? "\n") + `declare_id!("${programPkStr}")`;
+    });
+
+    return {
+      content: newContent,
+      updated,
+    };
+  };
+
+  const updateIdPython = (content: string) => {
+    let updated = false;
+
+    const pythonDeclareIdRegex = new RegExp(/^declare_id\(("|')(\w*)("|')\)/gm);
+
+    const newContent = content.replace(pythonDeclareIdRegex, (match) => {
+      const res = pythonDeclareIdRegex.exec(match);
+      if (!res) return match;
+      updated = true;
+      return `declare_id('${programPkStr}')`;
+    });
+
+    return {
+      content: newContent,
+      updated,
+    };
+  };
+
+  const getUpdatedProgramIdContent = (path: string) => {
+    let content = files[path].content;
+    let updated = false;
+    if (content) {
+      if (path.endsWith(".rs")) {
+        const updateIdResult = updateIdRust(content);
+        content = updateIdResult.content;
+        updated = updateIdResult.updated;
+      } else if (path.endsWith(".py")) {
+        const updateIdResult = updateIdPython(content);
+        content = updateIdResult.content;
+        updated = updateIdResult.updated;
+      }
+    }
+
+    return { content, updated };
+  };
+
+  // Prioritise files where we are likely to find a rust `declare_id!`
+  const prioritiseFilePaths = (files: ExplorerFiles) => {
+    const prioritised: Array<string> = [];
+    for (const path in files) {
+      if (path.endsWith("lib.rs") || path.endsWith("id.rs")) {
+        prioritised.unshift(path);
+      } else {
+        prioritised.push(path);
+      }
+    }
+    return prioritised;
+  };
+
+  const files = PgExplorer.files;
+  const prioritisedFilePaths = prioritiseFilePaths(files);
+  const buildFiles: TupleFiles = [];
+
+  let alreadyUpdatedId = false;
+  if (PgExplorer.isTemporary) {
+    for (const path of prioritisedFilePaths) {
+      let content;
+      // Shared files are already in correct format, we only update program id
+      if (!alreadyUpdatedId) {
+        const updateIdResult = getUpdatedProgramIdContent(path);
+        content = updateIdResult.content;
+        alreadyUpdatedId = updateIdResult.updated;
+      } else {
+        content = files[path].content;
+      }
+      if (!content) continue;
+      buildFiles.push([path, content]);
+    }
+  } else {
+    for (let path of prioritisedFilePaths) {
+      if (!path.startsWith(PgExplorer.getCurrentSrcPath())) continue;
+
+      let content = files[path].content;
+      if (!alreadyUpdatedId) {
+        const updateIdResult = getUpdatedProgramIdContent(path);
+        content = updateIdResult.content;
+        alreadyUpdatedId = updateIdResult.updated;
+      }
+      if (!content) continue;
+
+      // Remove the workspace from path because build only needs /src
+      path = path.replace(
+        PgExplorer.currentWorkspacePath,
+        PgExplorer.PATHS.ROOT_DIR_PATH
+      );
+
+      buildFiles.push([path, content]);
+    }
+  }
+
+  return buildFiles;
 };
