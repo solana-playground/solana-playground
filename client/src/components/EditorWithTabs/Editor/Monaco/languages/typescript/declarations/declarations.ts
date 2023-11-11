edbaba13421faa7207baa893267f8c6d14e6884e
acheron <acheroncrypto@gmail.com>
2023-11-11 16:33:15 +0100

    Lazy load `declarations.ts` instead of loading each file individually

--- client/src/components/EditorWithTabs/Editor/Monaco/languages/typescript/declarations/declarations.ts
+++ client/src/components/EditorWithTabs/Editor/Monaco/languages/typescript/declarations/declarations.ts
@@ -1,39 +1,29 @@
+import { declareDisposableTypes } from "./disposable";
+import { declareGlobalTypes } from "./global";
+import { declareImportableTypes } from "./importable";
 import type { Disposable } from "../../../../../../../utils/pg";
 
 /**
  * Initialize type declarations.
  *
  * Steps:
- * 1. Declare global types
+ * 1. Declare global and importable types
  * 2. Declare disposable types
- * 3. Declare importable types
  *
  * @returns a disposable to dispose all events
  */
 export const initDeclarations = async (): Promise<Disposable> => {
-  const [disposable, global, importable] = await Promise.all([
-    // Disposable declarations
-    (async () => {
-      const { declareDisposableTypes } = await import("./disposable");
-      return declareDisposableTypes();
-    })(),
-    // Global declarations
-    (async () => {
-      const { declareGlobalTypes } = await import("./global");
-      return await declareGlobalTypes();
-    })(),
-    // Importable declarations
-    (async () => {
-      const { declareImportableTypes } = await import("./importable");
-      return await declareImportableTypes();
-    })(),
+  const [global, importable] = await Promise.all([
+    declareGlobalTypes(),
+    declareImportableTypes(),
   ]);
+  const disposable = declareDisposableTypes();
 
   return {
     dispose: () => {
       global.dispose();
-      disposable.dispose();
       importable.dispose();
+      disposable.dispose();
     },
   };
 };
