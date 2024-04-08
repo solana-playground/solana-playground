13042b1f6207197a163177a39c70de83f621d68c
acheron <acheroncrypto@gmail.com>
2024-04-08 14:33:02 +0200

    Add ability to refresh children of `ErrorBoundary`

--- client/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ client/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,12 +1,17 @@
-import { Component, ErrorInfo, ReactNode } from "react";
+import { Component, CSSProperties, ErrorInfo, ReactNode } from "react";
 
+import Button from "../Button";
+import Tooltip from "../Tooltip";
 import Fallback from "./Fallback";
+import { Refresh } from "../Icons";
 
 interface Props {
   /** Node to render as children */
   children?: ReactNode;
   /** Fallback node when there is an error */
   Fallback?: (props: { error: Error }) => JSX.Element;
+  /** If truthy, add a refresh button to re-render the component */
+  refreshButton?: boolean | CSSProperties;
 }
 
 interface State {
@@ -47,11 +52,38 @@ class ErrorBoundary extends Component<Props, State> {
   /** Render `fallback` if there is an error, `children` otherwise. */
   render() {
     if (this.state.error) {
-      if (this.props.Fallback) {
-        return <this.props.Fallback error={this.state.error} />;
+      const FbComponent = this.props.Fallback ?? Fallback;
+      const FbElement = <FbComponent error={this.state.error} />;
+
+      if (this.props.refreshButton) {
+        return (
+          <div>
+            <div
+              style={{
+                ...(typeof this.props.refreshButton === "object"
+                  ? this.props.refreshButton
+                  : {}),
+              }}
+            >
+              <Tooltip element="Refresh">
+                <Button
+                  kind="icon"
+                  onClick={() => {
+                    // Reset the state (without the error) in order to re-render
+                    this.setState((s) => ({ ...s, error: null }));
+                  }}
+                >
+                  <Refresh color="error" />
+                </Button>
+              </Tooltip>
+            </div>
+
+            {FbElement}
+          </div>
+        );
       }
 
-      return <Fallback error={this.state.error} />;
+      return FbElement;
     }
 
     return this.props.children;
