ce86cb968f8317de0d00bd9c80a5442f05e73d35
acheron <acheroncrypto@gmail.com>
2024-06-07 07:57:43 +0200

    Reuse terminal formatted log functionality in `help` command

--- client/src/commands/help/help.ts
+++ client/src/commands/help/help.ts
@@ -1,40 +1,11 @@
-import { PgCommandManager, PgCommon, PgTerminal } from "../../utils/pg";
+import { formatCmdList, PgCommandManager, PgTerminal } from "../../utils/pg";
 import { createCmd } from "../create";
 
 export const help = createCmd({
   name: "help",
   description: "Print help message",
   run: () => {
-    const commandNames = PgCommon.keys(PgCommandManager.commands);
-
-    const helpMessage =
-      "COMMANDS:\n" +
-      commandNames
-        .sort((a, b) => {
-          // Put non-letter commands to the end
-          if (!/^[a-zA-Z-]+$/.test(PgCommandManager.commands[b].name)) {
-            return -1;
-          }
-
-          return a.localeCompare(b);
-        })
-        .reduce((acc, cmdName) => {
-          const cmd = PgCommandManager.commands[cmdName];
-
-          return (
-            acc +
-            "    " +
-            cmd.name +
-            fillWhitespace(cmd.name.length) +
-            cmd.description +
-            "\n"
-          );
-        }, "");
-
-    PgTerminal.log(helpMessage);
+    const cmds = Object.values(PgCommandManager.commands);
+    PgTerminal.log("Commands:\n\n" + formatCmdList(cmds));
   },
 });
-
-const fillWhitespace = (cmdLength: number) => {
-  return new Array(25 - cmdLength).fill(" ").reduce((acc, v) => acc + v);
-};
