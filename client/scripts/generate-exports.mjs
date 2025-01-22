f1a0dde0ce48a35cadcf45aeff5a253defaf1304
acheron <acheroncrypto@gmail.com>
2025-01-22 17:45:33 +0100

    Automatically export commands

--- client/scripts/generate-exports.mjs
+++ client/scripts/generate-exports.mjs
@@ -1,16 +1,25 @@
+// Generate file exports
+
 import fs from "fs/promises";
 import pathModule from "path";
 
 import { CLIENT_PATH } from "./utils.mjs";
 
-const dir = "block-explorers";
-const fileName = `${dir}.ts`;
-const dirPath = pathModule.join(CLIENT_PATH, "src", dir);
-const items = await fs.readdir(dirPath);
-const exports = items
-  .filter((item) => item !== fileName && item !== "index.ts")
-  .map(pathModule.parse)
-  .map((parsed) => parsed.name)
-  .reduce((acc, cur) => acc + `export * from "./${cur}";\n`, "");
+const DIRS = ["block-explorers", "commands"];
 
-await fs.writeFile(pathModule.join(dirPath, fileName), exports);
+for (const dir of DIRS) {
+  const dirPath = pathModule.join(CLIENT_PATH, "src", dir);
+  const items = await fs
+    .readdir(dirPath)
+    .then((items) => items.map(pathModule.parse));
+  const namesToSkip = [dir, "index"];
+  const hasDir = items.some((item) => !item.ext);
+  const exports = items
+    .filter(
+      (item) => !namesToSkip.includes(item.name) && (hasDir ? !item.ext : true)
+    )
+    .map((item) => item.name)
+    .reduce((acc, cur) => acc + `export * from "./${cur}";\n`, "");
+
+  await fs.writeFile(pathModule.join(dirPath, `${dir}.ts`), exports);
+}
