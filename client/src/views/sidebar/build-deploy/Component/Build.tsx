52a86b3fb930930396d20796c42d89e1770a7a97
acheron <acheroncrypto@gmail.com>
2025-03-12 20:33:34 +0100

    Show an info text if there is no project to build

--- client/src/views/sidebar/build-deploy/Component/Build.tsx
+++ client/src/views/sidebar/build-deploy/Component/Build.tsx
@@ -2,7 +2,8 @@ import { useCallback } from "react";
 import styled from "styled-components";
 
 import Button from "../../../../components/Button";
-import { useRenderOnChange } from "../../../../hooks";
+import Text from "../../../../components/Text";
+import { useExplorer, useRenderOnChange } from "../../../../hooks";
 import { PgCommand, PgGlobal } from "../../../../utils/pg";
 
 const Build = () => {
@@ -11,6 +12,11 @@ const Build = () => {
     PgGlobal.buildLoading
   );
   const build = useCallback(() => PgCommand.build.execute(), []);
+
+  const { explorer } = useExplorer();
+  if (!explorer.isTemporary && !explorer.currentWorkspaceName) {
+    return <Text>No project to build.</Text>;
+  }
 
   return (
     <Wrapper>
