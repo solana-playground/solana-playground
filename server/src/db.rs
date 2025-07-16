bfa301a6225152d30454589bace5082784d129f4
Jacob Creech <jacob.creech@solana.com>
2025-07-16 11:47:27 -0500

    chore: fmt

--- server/src/db.rs
+++ server/src/db.rs
@@ -12,16 +12,11 @@ static DB: OnceLock<PgPool> = OnceLock::new();
 ///
 /// NOTE: Other functions in this module will not be usable before this function is executed.
 pub async fn init(uri: &str, _name: String) -> Result<()> {
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(uri)
-        .await?;
-    
+    let pool = PgPoolOptions::new().max_connections(5).connect(uri).await?;
+
     // Run migrations
-    sqlx::migrate!("./migrations")
-        .run(&pool)
-        .await?;
-    
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
     DB.set(pool).map_err(|_| anyhow!("Failed to init `DB`"))
 }
 
@@ -29,13 +24,10 @@ pub async fn init(uri: &str, _name: String) -> Result<()> {
 pub async fn find_by_id(id: &str, collection: &str) -> Result<Option<Value>> {
     let pool = get_pool()?;
     let id = Uuid::parse_str(id)?;
-    
+
     let query = format!("SELECT data FROM {} WHERE id = $1", collection);
-    let row: Option<(Value,)> = sqlx::query_as(&query)
-        .bind(id)
-        .fetch_optional(pool)
-        .await?;
-    
+    let row: Option<(Value,)> = sqlx::query_as(&query).bind(id).fetch_optional(pool).await?;
+
     Ok(row.map(|(data,)| data))
 }
 
@@ -43,14 +35,17 @@ pub async fn find_by_id(id: &str, collection: &str) -> Result<Option<Value>> {
 pub async fn insert(value: Value, collection: &str) -> Result<String> {
     let pool = get_pool()?;
     let id = Uuid::new_v4();
-    
-    let query = format!("INSERT INTO {} (id, data) VALUES ($1, $2) RETURNING id", collection);
+
+    let query = format!(
+        "INSERT INTO {} (id, data) VALUES ($1, $2) RETURNING id",
+        collection
+    );
     let row: (Uuid,) = sqlx::query_as(&query)
         .bind(id)
         .bind(&value)
         .fetch_one(pool)
         .await?;
-    
+
     Ok(row.0.to_string())
 }
 
