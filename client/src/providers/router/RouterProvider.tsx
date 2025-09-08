90b9b222a9a0f6aa6f9e27f117c05c1b4abc3b5f
acheron <acheroncrypto@gmail.com>
2025-09-08 18:38:50 +0200

    Add `RouterProvider`

---
+++ client/src/providers/router/RouterProvider.tsx
@@ -0,0 +1,37 @@
+import { FC, useEffect } from "react";
+import { BrowserRouter, useLocation, useNavigate } from "react-router-dom";
+
+import { PgCommon, PgRouter } from "../../utils/pg";
+import { useGetStatic } from "../../hooks";
+
+export const RouterProvider: FC = ({ children }) => (
+  <BrowserRouter>
+    <InternalRouter>{children}</InternalRouter>
+  </BrowserRouter>
+);
+const InternalRouter: FC = ({ children }) => {
+  // Location
+  const location = useLocation();
+
+  // Path
+  useEffect(() => {
+    PgCommon.createAndDispatchCustomEvent(
+      PgRouter.events.ON_DID_CHANGE_PATH,
+      location.pathname
+    );
+  }, [location.pathname]);
+
+  // Hash
+  useEffect(() => {
+    PgCommon.createAndDispatchCustomEvent(
+      PgRouter.events.ON_DID_CHANGE_HASH,
+      location.hash
+    );
+  }, [location.hash]);
+
+  // Navigate
+  const navigate = useNavigate();
+  useGetStatic(PgRouter.events.NAVIGATE, navigate);
+
+  return <>{children}</>;
+};
