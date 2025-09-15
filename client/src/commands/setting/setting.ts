cb6c4fc2314374ab885c17ac0047f09a1b410a2d
acheron <acheroncrypto@gmail.com>
2025-09-15 18:32:43 +0200

    Rename `pg` command to `setting`

--- client/src/commands/setting/setting.ts
+++ client/src/commands/setting/setting.ts
@@ -2,20 +2,20 @@ import { PgCommon, PgSettings, PgTerminal } from "../../utils/pg";
 import { createArgs, createCmd, createSubcmd } from "../create";
 
 /** Common setting ID argument */
-const settingIdArg = {
+const idArg = {
   name: "id",
   description: "Setting ID",
   values: () => PgSettings.all.map((s) => s.id),
 } as const;
 
-export const pg = createCmd({
+export const setting = createCmd({
   name: "pg",
-  description: "Playground utilities",
+  description: "Manage playground settings",
   subcommands: [
     createSubcmd({
       name: "get",
       description: "Get setting value",
-      args: createArgs([settingIdArg]),
+      args: createArgs([idArg]),
       handle: (input) => {
         const value = PgSettings.get(input.args.id).getValue();
         PgTerminal.println(value);
@@ -26,7 +26,7 @@ export const pg = createCmd({
       name: "set",
       description: "Set setting value",
       args: createArgs([
-        settingIdArg,
+        idArg,
         {
           name: "value",
           description: "Value to set",
