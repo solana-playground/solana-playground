01993df958c92488c427da074742e3c3425f1f4f
acheron <acheroncrypto@gmail.com>
2025-10-23 21:21:35 +0200

    Show tutorial progress under tutorial's thumbnail

--- client/src/views/main/primary/Tutorials/TutorialCard.tsx
+++ client/src/views/main/primary/Tutorials/TutorialCard.tsx
@@ -1,38 +1,65 @@
-import { FC } from "react";
-import styled, { css } from "styled-components";
+import { FC, useState } from "react";
+import styled, { css, keyframes } from "styled-components";
 
 import Card from "../../../../components/Card";
 import Img from "../../../../components/Img";
 import Tag from "../../../../components/Tag";
-import { PgTheme, PgTutorial, TutorialData } from "../../../../utils/pg";
+import { useAsyncEffect } from "../../../../hooks";
+import {
+  PgTheme,
+  PgTutorial,
+  TutorialData,
+  TutorialMetadata,
+} from "../../../../utils/pg";
 
-const TutorialCard: FC<TutorialData> = ({
+type TutorialCardProps = TutorialData;
+
+const TutorialCard: FC<TutorialCardProps> = ({
   name,
   description,
   thumbnail,
   level,
   framework,
-}) => (
-  <Wrapper onClick={() => PgTutorial.open(name)}>
-    <ImgWrapper>
-      <TutorialImg src={thumbnail} />
-    </ImgWrapper>
+  pageCount,
+}) => {
+  const [metadata, setMetadata] = useState<TutorialMetadata>();
+  useAsyncEffect(async () => {
+    const tutorial = PgTutorial.all.find((t) => t.name === name);
+    if (!tutorial) throw new Error(`Tutorial not found: ${name}`);
+    const metadata = await PgTutorial.getMetadata(name);
+    setMetadata(metadata);
+  }, [name]);
 
-    <InfoWrapper>
-      <InfoTopSection>
-        <NameRow>
-          <Name>{name}</Name>
-          <Tag kind="level" value={level} />
-        </NameRow>
-        <Description>{description}</Description>
-      </InfoTopSection>
+  return (
+    <Wrapper onClick={() => PgTutorial.open(name)}>
+      <ImgWrapper
+        progress={
+          metadata
+            ? metadata.completed
+              ? 100
+              : ((metadata.pageNumber - 1) / pageCount) * 100
+            : 0
+        }
+      >
+        <TutorialImg src={thumbnail} />
+      </ImgWrapper>
 
-      <InfoBottomSection>
-        {framework && <Tag kind="framework" value={framework} />}
-      </InfoBottomSection>
-    </InfoWrapper>
-  </Wrapper>
-);
+      <InfoWrapper>
+        <InfoTopSection>
+          <NameRow>
+            <Name>{name}</Name>
+            <Tag kind="level" value={level} />
+          </NameRow>
+          <Description>{description}</Description>
+        </InfoTopSection>
+
+        <InfoBottomSection>
+          {framework && <Tag kind="framework" value={framework} />}
+        </InfoBottomSection>
+      </InfoWrapper>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled(Card)`
   --img-height: 13.1rem;
@@ -42,9 +69,30 @@ const Wrapper = styled(Card)`
   padding: 0;
 `;
 
-const ImgWrapper = styled.div`
-  width: 100%;
-  height: var(--img-height);
+const ImgWrapper = styled.div<{ progress: number }>`
+  ${({ theme, progress }) => css`
+    width: 100%;
+    height: var(--img-height);
+    position: relative;
+
+    &::after {
+      --progress-height: 0.25rem;
+
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: calc(-1 * var(--progress-height));
+      height: var(--progress-height);
+      background: ${progress === 100
+        ? `linear-gradient(90deg, ${theme.colors.state.success.color} 0%, ${
+            theme.colors.state.success.color + theme.default.transparency.high
+          } 100%)`
+        : `linear-gradient(90deg, ${theme.colors.default.primary} 0%, ${theme.colors.default.secondary} 100%)`};
+      animation: ${keyframes`from { width: 0; } to { width: ${progress}%; }`}
+        ${theme.default.transition.duration.long}
+        ${theme.default.transition.type} forwards;
+    }
+  `}
 `;
 
 const TutorialImg = styled(Img)`
