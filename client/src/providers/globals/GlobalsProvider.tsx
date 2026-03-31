0b1279f031c11b8a199eed8666dabf076f8c9554
acheron <acheroncrypto@gmail.com>
2026-03-31 15:20:29 +0200

    Make globals initialization recoverable

--- client/src/providers/globals/GlobalsProvider.tsx
+++ client/src/providers/globals/GlobalsProvider.tsx
@@ -7,27 +7,32 @@ import { useAsyncEffect } from "../../hooks";
 
 export const GlobalsProvider: FC = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+  const [, setError] = useState();
 
   useAsyncEffect(async () => {
     setLoading(true);
 
     try {
       const { dispose } = await initAll(GLOBALS);
-      setError("");
       return dispose;
     } catch (e: any) {
-      setError(
-        `Error during globals initialization: ${
-          e.message ?? "Unexpected error"
-        }`
-      );
+      // Error boundaries do not catch promise errors.
+      // See https://github.com/facebook/react/issues/11334
+      //
+      // As a workaround, the following line manually triggers a render error,
+      // which is then caught by the parent `ErrorBoundary` component.
+      setError(() => {
+        throw new Error(
+          `Error during globals initialization: ${
+            e.message ?? "Unexpected error"
+          }`
+        );
+      });
     } finally {
       setLoading(false);
     }
   }, []);
 
   if (loading) return <AppLoading />;
-  if (error) return <>{error}</>;
   return <>{children}</>;
 };
