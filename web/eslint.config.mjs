35d70d701b5a9934316d331e916becc8d7903515
Sergo <rogaldh@radsh.red>
2026-05-12 18:57:20 +0100

    chore(act): fix build issues

---
+++ web/eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        fetch: "readonly",
+        Request: "readonly",
+        Response: "readonly",
+        Headers: "readonly",
+        URL: "readonly",
+        TransformStream: "readonly",
+        ReadableStream: "readonly",
+        TextEncoder: "readonly",
+        setImmediate: "readonly",
+      },
+    },
+  },
+];
