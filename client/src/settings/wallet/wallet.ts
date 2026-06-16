31b65f7560e4fd2058564e6ab5e7c39099a975b1
acheron <acheroncrypto@gmail.com>
2026-06-16 07:57:35 +0200

    Clarify `wallet.automaticAirdrop` setting description

--- client/src/settings/wallet/wallet.ts
+++ client/src/settings/wallet/wallet.ts
@@ -4,7 +4,7 @@ export const wallet = [
   createSetting({
     id: "wallet.automaticAirdrop",
     description:
-      "Whether to automatically send airdrop requests based on the current endpoint",
+      "Whether to automatically send airdrop requests when balance is less than the cluster's maximum airdrop amount",
     default: true,
   }),
 ];
